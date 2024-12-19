642aa3cfc4faa4ec1a5e24f2ab44e8fd4459fb79
Chichigami <g.feng.work@gmail.com>
2024-12-19 02:59:51 -0500

    front end css, template, login finished

--- backend/main.go
+++ backend/main.go
@@ -7,24 +7,25 @@ import (
 func main() {
 	//connect to DB
 
-	r := gin.Default() //default page should be login page
+	router := gin.Default() //default page should be login page
+	router.Delims("{[{", "}]}")
 
-	r.GET("/ping", handlerPlaceholder)
+	router.GET("/ping", handlerPlaceholder)
 
-	login := r.Group("/login")
+	login := router.Group("/login")
 	{
 		login.POST("", handlerLoginNew)      //new login, need admin priv
 		login.GET("", handlerLogin)          //wait for login info, verifies
 		login.PUT("", handlerLoginUpdate)    //update login
 		login.DELETE("", handlerLoginDelete) //delete login, need admin priv
 	}
-	dashboard := r.Group("dashboard") //might just make it one handler. cache date in server. if date is today then grab cache?
+	dashboard := router.Group("/dashboard") //might just make it one handler. cache date in server. if date is today then grab cache?
 	{
 		dashboard.GET("", handlerDashboardToday)          //show today's dashboard
 		dashboard.GET("/:date", handlerDashboardNotToday) //show some date's dashboard (maybe yesterday)
 	}
 
-	patient := r.Group("patients")
+	patient := router.Group("/patients")
 	{
 		patient.POST("", handlerPatientNew)          //add new patient
 		patient.GET("/:ID", handlerPatient)          //show patient based on ID
@@ -33,7 +34,7 @@ func main() {
 		//patient.GET("/:ID", handlerPatientQuery)     //query for patient based on patient id, name, dob
 	}
 
-	charts := r.Group("charts")
+	charts := router.Group("/patients/charts")
 	{
 		charts.POST("/:ID", handlerChartNew)      //make new chart for patient
 		charts.GET("/:ID", handlerChart)          //show chart info
@@ -41,12 +42,12 @@ func main() {
 		charts.DELETE("/:ID", handlerChartDelete) //delete chart
 	}
 
-	schedule := r.Group("/schedule")
+	schedule := router.Group("/schedule")
 	{
 		schedule.POST("/:ID", handlerScheduleNew)      //schedule a patient
 		schedule.DELETE("/:ID", handlerScheduleDelete) //delete a patient's appointment
 	}
-	r.Run(":8000")
+	router.Run(":8000")
 }
 
 type config struct {
