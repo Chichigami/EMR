b7b8ad438169e3c3a51f4be78d54481aeec113b0
Chichigami <g.feng.work@gmail.com>
2025-02-06 21:58:47 -0500

    appointment table fixed

--- sql/queries/appointments.sql
+++ sql/queries/appointments.sql
@@ -1,6 +1,6 @@
 -- name: CreateAppointmentForPatient :one
 INSERT INTO appointments (
-    appointment_date, patient_id, reasoning
+    patient_id, appointment, reasoning
 ) 
 VALUES (
     $1, $2, $3
@@ -9,14 +9,14 @@ RETURNING *;
 
 -- name: DeleteAppointment :exec
 DELETE FROM appointments
-WHERE patient_id = $1 AND appointment_date = $2;
+WHERE id = $1;
 
 -- name: GetAppointmentsBasedOnDay :many
 SELECT *
 FROM appointments
-WHERE appointment_date = $1;
+WHERE appointment = $1;
 
 -- name: UpdateAppointment :exec
 UPDATE appointments
-SET appointment_date = $2, appointment_time = $3, updated_at = NOW()
+SET appointment = $2, updated_at = NOW()
 WHERE id = $1;
--- sql/schema/003_appointments.sql
+++ sql/schema/003_appointments.sql
@@ -3,8 +3,7 @@ CREATE TABLE appointments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
-    appointment_date DATE NOT NULL,
-    appointment_time TIMESTAMP NOT NULL,
+    appointment TIMESTAMP NOT NULL,
     patient_id SERIAL NOT NULL REFERENCES patients(patient_id),
     reasoning TEXT NULL
 );
