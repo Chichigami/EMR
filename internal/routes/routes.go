040b0a813fc681bbc6d23d31791c832a6869055a
Chichigami <g.feng.work@gmail.com>
2025-02-08 17:56:05 -0500

    case sensitive bug fixed, enabled appt del route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -33,10 +33,11 @@ func AddRoutes(r *gin.Engine, h *handlers.HandlerConfig) {
 	//start of api endpoints
 	login := r.Group("/login")
 	{
-		login.GET("", h.HandlerUsersCreate)    //create new login if admin auth
-		login.POST("", h.HandlerUsersRead)     //wait for login info, verifies
-		login.PUT("", h.HandlerUsersUpdate)    //update login
-		login.DELETE("", h.HandlerUsersDelete) //delete login, need admin priv
+		login.GET("", h.HandlerUsersCreate) //create new login if admin auth
+		login.POST("", h.HandlerUsersRead)  //wait for login info, verifies
+		login.PUT("", h.HandlerUsersUpdate) //update login
+		//login.DELETE("", h.HandlerUsersDelete) //delete login, need admin priv
+		login.DELETE("", h.HandlerDeleteAllUser)
 	}
 
 	// dashboard := r.Group("/dashboard")
@@ -47,24 +48,24 @@ func AddRoutes(r *gin.Engine, h *handlers.HandlerConfig) {
 	patient := r.Group("/patients")
 	{
 		patient.POST("/create", h.HandlerPatientsCreate) //add new patient
-		patient.GET("/:ID", h.HandlerPatientsRead)       //show patient based on ID
-		//patient.PUT("/:ID", h.HandlerPatientsUpdate)     //update patient info
-		patient.DELETE("/:ID", h.HandlerPatientsDelete) //delete existing patient, need admin perm
-		//patient.GET("/:ID", handlerPatientQuery)     //query for patient based on patient id, name, dob
+		patient.GET("/:id", h.HandlerPatientsRead)       //show patient based on id
+		//patient.PUT("/:id", h.HandlerPatientsUpdate)     //update patient info
+		patient.DELETE("/:id", h.HandlerPatientsDelete) //delete existing patient, need admin perm
+		//patient.GET("/:id", handlerPatientQuery)     //query for patient based on patient id, name, dob
 	}
 
 	// charts := r.Group("/patients/charts")
 	// {
-	// 	charts.POST("/:ID", h.HandlerChartsCreate)   //make new chart for patient
-	// 	charts.GET("/:ID", h.HandlerChartsRead)      //show chart info
-	// 	charts.PUT("/:ID", h.HandlerChartsUpdate)    //update chart
-	// 	charts.DELETE("/:ID", h.HandlerChartsDelete) //delete chart
+	// 	charts.POST("/:id", h.HandlerChartsCreate)   //make new chart for patient
+	// 	charts.GET("/:id", h.HandlerChartsRead)      //show chart info
+	// 	charts.PUT("/:id", h.HandlerChartsUpdate)    //update chart
+	// 	charts.DELETE("/:id", h.HandlerChartsDelete) //delete chart
 	// }
 
 	schedule := r.Group("/schedule")
 	{
 		schedule.POST("/create", h.HandlerAppointmentsCreate) //schedule a patient
-		//schedule.DELETE("/:ID", h.HandlerAppointmentsDelete) //delete a patient's appointment
+		schedule.DELETE("/:id", h.HandlerAppointmentsDelete)  //delete a patient's appointment
 	}
 }
 
