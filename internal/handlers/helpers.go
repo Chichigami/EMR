c968846d936a64d9e9f102d9fa94096864a4aea8
Chichigami <g.feng.work@gmail.com>
2025-02-09 18:46:27 -0500

    feat: string to int32 helper; refractor: name of nullstring to be more conventional

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -2,9 +2,10 @@ package handlers
 
 import (
 	"database/sql"
+	"strconv"
 )
 
-func NullStringCheck(s string) sql.NullString {
+func NullString(s string) sql.NullString {
 	return sql.NullString{
 		String: s,
 		Valid:  s != "",
@@ -16,4 +17,12 @@ func NullInt32(i int32) sql.NullInt32 {
 		Int32: i,
 		Valid: i != 0,
 	}
+}
+
+func ConvertStringToInt32(s string) (int32, error) {
+	d, err := strconv.Atoi(s)
+	if err != nil {
+		return -1, err
+	}
+	return int32(d), nil
 }
