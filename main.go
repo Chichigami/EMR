bf18f4e522f257b5ca34c6a2dc20b66041a59b74
Chichigami <g.feng.work@gmail.com>
2025-02-20 20:51:31 -0500

    feat: cron job and comment

--- main.go
+++ main.go
@@ -27,11 +27,20 @@ func main() {
 	routes.AddRoutes(router, h)
 
 	c := cron.New()
-	defer c.Stop()
 
-	_, err := c.AddFunc("6 * * * *", h.CacheToday)
+	// This cron job refreshes the dashboard cache every start of day to ensure the latest appointments are included.
+	//
+	// Why always refresh?
+	// - New appointments may be added after the last cache update but before the start of the day.
+	// - Avoids checks and calls between Redis cache, the appointments table, and the dashboards table.
+	// - Performance optimizations from comparison are uncertain, so a full refresh is simpler and reliable.
+	//
+	// If the cron job fails to register, the program exits with a fatal error.
+	_, err := c.AddFunc("* * * * *", func() {
+		h.CacheDashboard(time.Now())
+	})
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln(err)
 	}
 
 	server := &http.Server{
@@ -39,6 +48,9 @@ func main() {
 		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
+
+	c.Start()
+	defer c.Stop()
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
